b54e9c3b1691e308e03d974be49dbef9bdaeac19
Abhishek Gupta <80100564+abhigupta24@users.noreply.github.com>
2023-01-13 02:19:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg'; 
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
-// import SideBar from './Components/SideBar';
 
 function App() {
   return (
